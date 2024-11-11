236877dfdd66261a0a9be04ebb2de79d7df1f2f3
Stanislav <salekkaexe@gmail.com>
2024-11-11 19:53:33 +0400

    Update PHP_Week1_Task4.php

--- PHP_Week1_Task4.php
+++ PHP_Week1_Task4.php
@@ -1,17 +1,19 @@
 <?php
 //Проверка числа на четность
-function checkChet(int $number)
+function checkChet(int $number): string
 {
     if ($number % 2 == 0) 
     {
-        echo "Число $number является четным"; //Число четное
+        return "Число $number является четным"; //Число четное
     } 
     else
     {
-        echo "Число $number является нечетным"; //Число нечетное
+        return "Число $number является нечетным"; //Число нечетное
     }
 }
-//Использование функции
+
+// Использование функции
 $number = 4;
-checkChet($number);
+$result = checkChet($number); //Сохраняем результат в переменной
+echo $result; //Выводим результат
 ?>
