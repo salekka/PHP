8c55647f682778768e2968f140a7914acc054f9b
Stanislav <salekkaexe@gmail.com>
2024-11-11 18:51:49 +0400

    Update PHP_Week1_Task2.php

--- PHP_Week1_Task2.php
+++ PHP_Week1_Task2.php
@@ -1,11 +1,11 @@
 <?php
 //Получение длины строки
-function StringLength(string $string) 
+function stringLength(string $string) 
 {
     $length = strlen($string); //strlen — Получает длину строки, но лучше использовать mb_strlen так как он считает кириллицу более корреткно 
     echo "Длина строки '$string' составляет $length символов."; //Выводит результат
 }
 //Использования функции
 $string = "Task_2"; 
-StringLength($string);
+stringLength($string);
 ?>
