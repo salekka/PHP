e520b68cb7f901e089fd4c87ee33607389465274
Stanislav <salekkaexe@gmail.com>
2024-11-11 20:01:08 +0400

    Update PHP_Week1_Task5.php

--- PHP_Week1_Task5.php
+++ PHP_Week1_Task5.php
@@ -1,23 +1,24 @@
 <?php
 //Проверка совпадения первых букв двух слов
-function checkWords(string $word1, string $word2) 
+function checkWords(string $word1, string $word2): string 
 {
     $firstword = substr($word1, 0, 1);
     $secondword = substr($word2, 0, 1);
 
-    //Сравнение первых буквв
-    if (strtolower($firstword) == strtolower($secondword)) //strtolower — Приводит строку к нижнему регистру(преобразуются в строчную букву, которая соответствует заглавной)
+    // Сравнение первых букв
+    if (strtolower($firstword) == strtolower($secondword)) //strtolower — Приводит строку к нижнему регистру
     {
-        echo "Первые буквы слов '$word1' и '$word2' совпадают: '$firstword'";
+        return "Первые буквы слов '$word1' и '$word2' совпадают: '$firstword'";
     } 
     else 
     {
-        echo "Первые буквы слов '$word1' и '$word2' не совпадают: '$firstword' и '$secondword'";
+        return "Первые буквы слов '$word1' и '$word2' не совпадают: '$firstword' и '$secondword'";
     }
 }
 
 //Использование функции
 $word1 = "Hello"; 
 $word2 = "Henry";
-checkWords($word1, $word2);
+$result = checkWords($word1, $word2); //Сохраняет результат в переменной
+echo $result; // Выводит результат
 ?>
